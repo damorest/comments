0a97a73c93a707d05e2e7c7059bdc5592514386e
damorest <pptreiding@gmail.com>
2024-09-25 19:57:13 +0300

    initial commit

---
+++ lib/consts/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color.fromRGBO(38, 136, 235, 1);
+const Color textColor = Colors.black87;
+const Color darkFontGrey = Color.fromRGBO(62, 68, 71, 1);
+const Color lightGrey = Color.fromRGBO(239, 239, 239, 1);
+const Color whiteColor = Colors.white;
+const Color cardColor = Color.fromRGBO(164, 205, 247, 1);
+const Color redColor = Colors.redAccent;
